f9c1873a195e5a4dfa4da10fe4cd1378f793eb8c
adriana0198 <adrianalucia110198@gmail.com>
2018-09-06 06:41:42 -0500

    sexto cambio

--- js/nuevo.js
+++ js/nuevo.js
@@ -1,7 +1,41 @@
 const navBar = document.querySelector('nav')
+var elemsimg = document.querySelectorAll('.materialboxed');
 window.addEventListener('scroll', () =>{
     if(window.scrollY>0) navBar.classList.add('is-down')
     else navBar.classList.remove('is-down')
-})
+});
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, {
+      height:500,  
+    });
+});
+
+
+var elemsimg = M.Materialbox.init(elemsimg);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel')
+  
+   
+    var arrowright = document.querySelector('.arrowright')
+    var arrowleft = document.querySelector('.arrowleft')
+    var instance = M.Carousel.init(elems, {
+      fullWidth: true,
+      indicators: true
+    })
+    
+    arrowright.addEventListener('click', () => {
+      var instances = M.Carousel.getInstance(elems[0])
+      instances.next()
+    })
+    arrowleft.addEventListener('click', () => {
+      var instances = M.Carousel.getInstance(elems[0])
+      instances.prev()
+    })
+   
+    
+  })
+  
 
