9d0da46792625a4bb75ed87c47671c3636d9f248
Aprendiz <Aprendiz@SGLDFPCAT7F570.FormacionSena.RED>
2018-09-19 07:25:07 -0500

    septimo cambio

--- js/nuevo.js
+++ js/nuevo.js
@@ -1,65 +1,69 @@
+const navBar = document.querySelector('nav')
+var elemsimg = document.querySelectorAll('.materialboxed');
 window.addEventListener('scroll', () =>{
-  if(window.scrollY>0) navBar.classList.add('is-down')
-  else navBar.classList.remove('is-down')
+    if(window.scrollY>0) navBar.classList.add('is-down')
+    else navBar.classList.remove('is-down')
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.slider');
-  var instances = M.Slider.init(elems, {
-    height:500,  
-  });
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, {
+      height:500,  
+    });
 });
 
 
 var elemsimg = M.Materialbox.init(elemsimg);
 
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.carousel')
-
- 
-  var arrowright = document.querySelector('.arrowright')
-  var arrowleft = document.querySelector('.arrowleft')
-  var instance = M.Carousel.init(elems, {
-    fullWidth: true,
-    indicators: true
-  })
+    var elems = document.querySelectorAll('.carousel')
   
-  arrowright.addEventListener('click', () => {
-    var instances = M.Carousel.getInstance(elems[0])
-    instances.next()
-  })
-  arrowleft.addEventListener('click', () => {
-    var instances = M.Carousel.getInstance(elems[0])
-    instances.prev()
-  })
+   
+    var arrowright = document.querySelector('.arrowright')
+    var arrowleft = document.querySelector('.arrowleft')
+    var instance = M.Carousel.init(elems, {
+      fullWidth: true,
+      indicators: true
+    })
+    
+    arrowright.addEventListener('click', () => {
+      var instances = M.Carousel.getInstance(elems[0])
+      instances.next()
+    })
+    arrowleft.addEventListener('click', () => {
+      var instances = M.Carousel.getInstance(elems[0])
+      instances.prev()
+    })
 
 $('.light-box1').hide();
 $('#open1').click(function(){
-  $('.light-box1').show();
+    $('.light-box1').show();
 })
 $('#open4').click(function(){
-  $('.light-box1').show();
+    $('.light-box1').show();
 })
 
 $('#close1').click(function(){
-  $('.light-box1').hide();
+    $('.light-box1').hide();
 })
 $('.light-box2').hide();
 $('#open2').click(function(){
-  $('.light-box2').show();
+    $('.light-box2').show();
 })
 $('#close2').click(function(){
-  $('.light-box2').hide();
+    $('.light-box2').hide();
 })
 $('.light-box3').hide();
 $('#open3').click(function(){
-  $('.light-box3').show();
+    $('.light-box3').show();
 })
 $('#close3').click(function(){
-  $('.light-box3').hide();
+    $('.light-box3').hide();
 })
-})
+  })
 
+
+  
 
 
 
